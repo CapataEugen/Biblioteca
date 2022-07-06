90dc046cdc4e7ddf1f7a3ff824bb070b334fda07
Mihai Voina <mihaitza1058@yahoo.com>
2022-07-06 11:39:19 +0300

    Library

---
+++ Library/Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+public sealed class Library
+{
+    private UserManager usermanager = new UserManager();
+    private BookManager bookmanager = new BookManager();
+    public bool Login(string email, int password)
+    {
+        return usermanager.Login(email, password);
+    }
+
+    public bool Register(string email, int password)
+    {
+        return usermanager.Register(email, password);
+    }
+
+    public void ListBooks()
+    {
+        bookmanager.ListBooks();
+    }
+
+    public void AddBook(string bookname, string bookauthor)
+    {
+        bookmanager.AddBook(bookname, bookauthor);
+
+    }
+
+    public bool Rent(string bookname)
+    {
+        return bookmanager.Rent(bookname);
+    }
+
+    public bool Return()
+    {
+        return bookmanager.Rent();
+    }
+}
