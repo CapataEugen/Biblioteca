e3e69339836e50bea1059fed394c3891719e115d
Amalia Capatina <92114298+amalia6@users.noreply.github.com>
2022-07-06 11:48:53 +0300

    constructor for book

---
+++ Book/Book.cs
@@ -0,0 +1,12 @@
+namespace Book;
+class Book
+{
+    public string _name;
+    public string _author;
+    public Book(string name, string author)
+    {
+        _name = name;
+        _author = author;
+    }
+
+}
